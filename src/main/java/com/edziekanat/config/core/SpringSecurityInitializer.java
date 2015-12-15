0e84a31f8aea2bac30dbe87f573ea1bc62ee2e89
MuskalaMarcin <marcin.muskala@gmail.com>
2015-12-15 22:47:41 +0100

    setting up spring project

---
+++ src/main/java/com/edziekanat/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.edziekanat.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
