9fe2f73f9e5cbcb88a53404fa435bb209b43e49b
Marcin Muskala <marcin.muskala@gmail.com>
2016-04-11 13:03:26 +0200

    resolving maven problem with jpa descriptor and adding backend validation to password length

--- src/edziekanat/controller/common/SetNewPassword.java
+++ src/edziekanat/controller/common/SetNewPassword.java
@@ -38,23 +38,32 @@ public class SetNewPassword extends HttpServlet
 	String password = request.getParameter("password");
 	String passwordRepeat = request.getParameter("passwordrepeat");
 
-	if(password.equals(passwordRepeat))
+	if (password.equals(passwordRepeat))
 	{
-	    PasswordResetDAO passwordResetDAO = new PasswordResetDAO();
-	    PasswordResetDTO passwordResetDTO = passwordResetDAO.getEntity(resetId);
-	    passwordResetDTO.setIsActive(0);
-	    passwordResetDAO.update(passwordResetDTO);
+	    if (password.length() >= 5)
+	    {
+		PasswordResetDAO passwordResetDAO = new PasswordResetDAO();
+		PasswordResetDTO passwordResetDTO = passwordResetDAO.getEntity(resetId);
+		passwordResetDTO.setIsActive(0);
+		passwordResetDAO.update(passwordResetDTO);
 
-	    UserDTO user = passwordResetDTO.getUser();
-	    String salt = PasswordUtils.generateSalt();
-	    String hash = PasswordUtils.getSHA512PasswordHash(password, salt);
-	    user.setPassword(hash);
-	    user.setSalt(salt);
-	    new UserDAO().update(user);
+		UserDTO user = passwordResetDTO.getUser();
+		String salt = PasswordUtils.generateSalt();
+		String hash = PasswordUtils.getSHA512PasswordHash(password, salt);
+		user.setPassword(hash);
+		user.setSalt(salt);
+		new UserDAO().update(user);
 
-	    request.setAttribute("msgshort", "Zmieniono has³o");
-	    request.setAttribute("msglong", "Has³o zosta³o zmienione. Mo¿esz zalogowaæ siê do systemu.");
-	    request.getRequestDispatcher("common/info.jsp").forward(request, response);
+		request.setAttribute("msgshort", "Zmieniono has³o");
+		request.setAttribute("msglong", "Has³o zosta³o zmienione. Mo¿esz zalogowaæ siê do systemu.");
+		request.getRequestDispatcher("common/info.jsp").forward(request, response);
+	    }
+	    else
+	    {
+		request.setAttribute("errorshort", "B³±d");
+		request.setAttribute("errorlong", "Minimalna d³ugo¶æ has³a to 5 znaków.");
+		request.getRequestDispatcher("common/error.jsp").forward(request, response);
+	    }
 	}
 	else
 	{
